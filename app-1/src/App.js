11b2b49b9990d0d84e09de67bdcc50fd566f4f0d
Ali_Ahmadiavin <ahmadiavin@gmail.com>
2019-07-05 16:05:35 -0500

    fixed 1-7

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,49 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
-import Login from "./component/Login";
-import Image from "./component/Image"
-import List from "./component/List"
-import Todobutton from "./component/Todobutton"
 
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state ={
-      stuff: ['code', 'sleep'],
-      newItem: ""
-    }
-    this.addToStuff = this.addToStuff.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+  constructor() {
+    super();
+    this.state = {
+      message: ""
+    };
   }
 
-  addToStuff(item) {
-    this.setState({
-      stuff: this.state.stuff.concat([item])
-    })
+  handleChange(value) {
+    this.setState({message:value})
   }
 
-  handleChange(event) {
-    this.setState({
-      newItem: event.target.value
-    })
-  }
   render() {
-    return (
+    return(
       <div className="App">
-        <Login />
-
-        <input value={this.state.newItem} onChange={this.handleChange} />
-        <Todobutton action={this.addToStuff.bind(this, this.state.newItem)}>
-         add stuff
-        </Todobutton>
-
-
-        <List title="stuff to do" list={this.state.stuff} />
-
-
-
-        <Image url={ "https://http.cat/100" } />
+        <input onChange={e => this.handleChange(e.target.value)} type="text"></input>
+        <p>{this.state.message}</p>
+       
       </div>
     );
   }
--- app-1/src/component/Todobutton.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-export default function Button(props) {
-    return (
-        <button onClick={props.action}>
-            
-            {props.children}
-        </button>
-    )
-}
